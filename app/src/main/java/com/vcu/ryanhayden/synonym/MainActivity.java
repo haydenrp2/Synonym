685be6245fb582e2a8c55766230947f6ead90beb
haydenrp2 <haydenrp2@mymail.vcu.edu>
2018-03-31 16:17:17 -0400

    implemented search functionality into gui

--- app/src/main/java/com/vcu/ryanhayden/synonym/MainActivity.java
+++ app/src/main/java/com/vcu/ryanhayden/synonym/MainActivity.java
@@ -4,9 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    DatabaseManager db = new DatabaseManager(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +16,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonClick(View v){
         if(v.getId() == R.id.findSynonymButton){
+            EditText input = (EditText)findViewById(R.id.searchSynonym);
+            String inputString = input.getText().toString();
             Intent intent = new Intent(MainActivity.this, Results.class);
+            String result = db.searchByFirst(inputString);
+            intent.putExtra("resultString",result);
             startActivity(intent);
         }
         else if(v.getId() == R.id.enterValuesButton){
--- app/src/main/java/com/vcu/ryanhayden/synonym/Results.java
+++ app/src/main/java/com/vcu/ryanhayden/synonym/Results.java
@@ -2,11 +2,14 @@ package com.vcu.ryanhayden.synonym;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Results extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.results);
+        TextView tv = (TextView)findViewById(R.id.resultDisplay);
+        tv.setText(getIntent().getStringExtra("resultString"));
     }
 }
