7d62e8731bba30881023130d5540edf44fa34218
haydenrp2 <haydenrp2@mymail.vcu.edu>
2018-03-31 15:46:11 -0400

    added synonym pair object

---
+++ app/src/main/java/com/vcu/ryanhayden/synonym/SynonymPair.java
@@ -0,0 +1,27 @@
+package com.vcu.ryanhayden.synonym;
+
+public class SynonymPair {
+    private String firstWord;
+    private String secondWord;
+
+    public SynonymPair(String firstWord, String secondWord) {
+        this.firstWord = firstWord;
+        this.secondWord = secondWord;
+    }
+
+    public String getFirstWord() {
+        return firstWord;
+    }
+
+    public void setFirstWord(String firstWord) {
+        this.firstWord = firstWord;
+    }
+
+    public String getSecondWord() {
+        return secondWord;
+    }
+
+    public void setSecondWord(String secondWord) {
+        this.secondWord = secondWord;
+    }
+}
