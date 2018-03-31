ab9baa2fb2dba03ebd801d3357491ff74141bf0c
haydenrp2 <haydenrp2@mymail.vcu.edu>
2018-03-31 15:49:12 -0400

    added results activity

---
+++ app/src/main/java/com/vcu/ryanhayden/synonym/Results.java
@@ -0,0 +1,12 @@
+package com.vcu.ryanhayden.synonym;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Results extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.results);
+    }
+}
