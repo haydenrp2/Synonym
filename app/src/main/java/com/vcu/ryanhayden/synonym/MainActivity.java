92d1cc9d98dbd191c90caf8d7cbf6e4dd99053d7
haydenrp2 <haydenrp2@mymail.vcu.edu>
2018-03-31 15:54:41 -0400

    Added page navigation functionality

--- app/src/main/java/com/vcu/ryanhayden/synonym/MainActivity.java
+++ app/src/main/java/com/vcu/ryanhayden/synonym/MainActivity.java
@@ -1,7 +1,9 @@
 package com.vcu.ryanhayden.synonym;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View v){
+        if(v.getId() == R.id.findSynonymButton){
+            Intent intent = new Intent(MainActivity.this, Results.class);
+            startActivity(intent);
+        }
+        else if(v.getId() == R.id.enterValuesButton){
+            Intent intent = new Intent(MainActivity.this, EnterValues.class);
+            startActivity(intent);
+        }
     }
 }
