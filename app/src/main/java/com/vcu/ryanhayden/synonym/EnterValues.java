cd788a05723f811c930bc5435de5d0090195d90f
haydenrp2 <haydenrp2@mymail.vcu.edu>
2018-03-31 15:48:19 -0400

    added enter values activity

---
+++ app/src/main/java/com/vcu/ryanhayden/synonym/EnterValues.java
@@ -0,0 +1,12 @@
+package com.vcu.ryanhayden.synonym;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EnterValues extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enter_values);
+    }
+}
